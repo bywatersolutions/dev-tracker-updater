03472477838bd06578b32756e965cc4400f3d9bd
Kyle M Hall <kyle@bywatersolutions.com>
2019-11-05 10:06:12 -0500

    Increase the time between Bugzilla requests from 1 second to 10 seconds

--- tracker-updater.pl
+++ tracker-updater.pl
@@ -306,7 +306,7 @@ foreach my $track ( @$results ) {
         }
     };
 
-    sleep(1);    # pause for 1 second between requests so we don't kill RT and/or Bugzilla
+    sleep(10);    # pause for 10 second between requests so we don't kill RT and/or Bugzilla
 }
 
 $ua->post(
